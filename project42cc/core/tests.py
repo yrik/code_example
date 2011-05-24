ed9d893a8cc498d94394cd3e1e91a998308d030b
martin_iden <iurii.kriachko@gmail.com>
2011-05-24 16:19:11 +0300

    ticket:5 ready

--- project42cc/core/tests.py
+++ project42cc/core/tests.py
@@ -16,6 +16,9 @@ class ViewsTest(unittest.TestCase):
         self.user = User.objects.create_user('username',
             'user@mail.com', 'password')
 
+    def tearDown(self):
+        self.user.delete()
+
     def test_index(self):
         # Issue a GET request.
         url = reverse('core.views.index')
@@ -25,28 +28,29 @@ class ViewsTest(unittest.TestCase):
 
     def test_login(self):
         url = reverse('django.contrib.auth.views.login')
-        response = self.client.post(url, {
+        self.client.post(url, {
             'username': 'username',
             'password': 'password',
             })
-        # Check that the response is 200 OK.
-        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(self.user.is_authenticated(), True)
 
     def test_add_person(self):
         self.client.login(username='username', password='password')
-        url = reverse('core.views.index')
+        url = reverse('core.views.add_person')
         response = self.client.post(url, {
-            'name': 'Name',
-            'surname': 'Surname'
+            'name': 'AddName',
+            'surname': 'AddSurname'
             })
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
         # Check that the Person is present in db
         try:
-            person = Person.objects.get(name='Name', surname='Surname')
+            person = Person.objects.get(name__exact='AddName',
+                            surname__exact='AddSurname')
         except Person.DoesNotExist:
             person = None
-        self.assertiNotEqual(person, None)
+        self.assertNotEqual(person, None)
 
 
 class ModelsTest(unittest.TestCase):
@@ -70,7 +74,8 @@ class TemplateTagTest(unittest.TestCase):
 
 class ContextTest(unittest.TestCase):
     def test_context(self):
-        person = Person.objects.create(name='Name', surname='Surname')
+        person = Person.objects.create(name='ContextName',
+                 surname='ContextSurname')
         person = Person.objects.get(pk=1)
         template = Template('{% load customtags %}{% edit_link person %}')
         context = Context({'person': person})
