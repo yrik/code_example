8c3a7cb665c924a66468b906d5cec360d4c1b7c5
martin_iden <iurii.kriachko@gmail.com>
2011-05-24 20:05:07 +0300

    ticket:5 add fuctionality changed to edit

--- project42cc/core/urls.py
+++ project42cc/core/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import *
 
-from core.views import index, add_person
+from core.views import index, edit_person
 
 
 urlpatterns = patterns('',
     (r'^$', index),
-    (r'^add_person/$', add_person),
+    (r'^edit_person/$', edit_person),
     (r'^login/$', 'django.contrib.auth.views.login',
                         {'template_name': 'login.html'}),
 )
